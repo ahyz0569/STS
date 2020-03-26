a04ba5d30f0ffa2b7d2682864bf7119569c92962
JKS <juskus0310@naver.com>
2020-03-26 14:27:48 +0900

    DB연동 확인을 위한 메소드 호출

--- finalproject/src/main/java/com/multicampus/finalproject/controller/TestController.java
+++ finalproject/src/main/java/com/multicampus/finalproject/controller/TestController.java
@@ -1,12 +1,30 @@
 package com.multicampus.finalproject.controller;
 
+import java.util.List;
+
+import com.multicampus.finalproject.model.UserInfo;
+import com.multicampus.finalproject.service.UserInfoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController{
+
+    @Autowired
+    UserInfoService userInfoService;
+
+
     @RequestMapping("/")
     public String home(){
         return "home";
+    }
+
+    //Database연동 성공 확인하기
+    @RequestMapping("/query")
+    public @ResponseBody List<UserInfo> query() throws Exception{
+        return userInfoService.getAll();
     }
 }
