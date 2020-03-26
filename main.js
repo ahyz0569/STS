e48f70ecabb118483776f5f59871e84dd0bf250d
JungHyukJin <hyukjin4976@naver.com>
2020-03-26 17:47:32 +0900

    singUp.html 1. 전체 틀 설계 및 구현 2. 로그인/회원가입 페이지 커서 자동 포커스 설정 main.page 1. 헤더 우측 상단에 아이콘 추가.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // 햄버거 메뉴바 열기
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
-  document.getElementById("main__header").style.backgroundColor = "white";
+  document.getElementById("mySidenav").style.width = "220px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+  document.getElementById("main__header").style.backgroundColor = "#fdfdfd";
 }
 
 // 햄버거 메뉴바 닫기
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = "white";
+  document.body.style.backgroundColor = "#fdfdfd";
 }
