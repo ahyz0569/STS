d3681ebc5d699d04beca506068b761d5acf507ff
JungHyukJin <hyukjin4976@naver.com>
2020-04-01 11:50:25 +0900

    1.header 색상 2. camera.html 추가 3. 전체적인 반응형 수정

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // 햄버거 메뉴바 열기
 function openNav() {
-  document.querySelector('body').style.background = "#cccccc";
+  document.querySelector("body").style.background = "#ecf0f1";
   document.querySelector(".sidenav").style.width = "250px";
 }
 // 햄버거 메뉴바 닫기
 function closeNav() {
   document.querySelector(".sidenav").style.width = "0";
-  document.querySelector('body').style.background = "white";
+  document.querySelector("body").style.background = "white";
 }
