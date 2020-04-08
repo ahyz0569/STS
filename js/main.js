6dfb2666ddfc94832af38b5e4dc68af0cfab4483
JungHyukJin <hyukjin4976@naver.com>
2020-04-08 22:11:49 +0900

    result페이지 기본틀 설계

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 // 햄버거 메뉴바 열기
 function openNav() {
   document.querySelector(".sidenav").style.width = "250px";
-  document.querySelector("body").style.backgroundColor = "#ecf0f1";
   document.getElementById("bg").style.display = "block";
 }
+
 // 햄버거 메뉴바 닫기
 function closeNav() {
   document.querySelector(".sidenav").style.width = "0";
-  document.querySelector("body").style.backgroundColor = "#fdfdfd";
   document.getElementById("bg").style.display = "none";
 }
