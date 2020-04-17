2ea7c31ab8d534a3ce6efe1aba0fa66c84f10d75
Junnnnnnnnnnn <wjsguswls71@naver.com>
2020-04-17 17:36:40 +0900

    북마크 기능 추가(fetchAPI)

--- finalproject/src/main/java/com/multicampus/finalproject/controller/TestController.java
+++ finalproject/src/main/java/com/multicampus/finalproject/controller/TestController.java
@@ -4,17 +4,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import com.multicampus.finalproject.model.BookmarkVO;
 import com.multicampus.finalproject.model.JsonVO;
 import com.multicampus.finalproject.model.LabelJsonVO;
-import com.multicampus.finalproject.model.UserInfo;
+import com.multicampus.finalproject.model.SecurityUserInfo;
+import com.multicampus.finalproject.service.BookmarkService;
 import com.multicampus.finalproject.service.RestTemplateService;
 import com.multicampus.finalproject.service.UserInfoService;
 
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,8 +35,10 @@ public class TestController{
 
     @Autowired
     RestTemplateService restTemplateService;
-
     
+    @Autowired
+    BookmarkService bookmarkService;
+
     @RequestMapping("/")
     public String home(){
         return "main";
@@ -95,9 +102,29 @@ public class TestController{
         System.out.println("추천 전: " + name);
         ResponseEntity<LabelJsonVO> recomandResult = restTemplateService.getRecomandData(name);
         ArrayList<String> recomandList = recomandResult.getBody().getRecomandResult();
-        
+        System.out.println("recipeID" + recomandList);
         model.addAttribute("recipe", recomandList);
         return "resultRecipe";
     }
-    
+    @RequestMapping(value="/testFetch", method=RequestMethod.POST)
+    @ResponseBody public BookmarkVO requestMethodName(@RequestBody BookmarkVO resBody, @AuthenticationPrincipal SecurityUserInfo securityUserInfo) {
+        //세션에 저장 되어 있는 id를 가져옴
+        String userID = securityUserInfo.getUsername();
+        //브라우저에서 json으로 넘어온 레시피 id를 int로 변환
+        int recipeID = resBody.getRecipeID();
+        //체크 되었는지 안되었는지 확인 할 수 있는 변수
+        //db로 보낼 VO객체 생성
+        BookmarkVO bookmarkVO = new BookmarkVO(userID, recipeID);
+
+        if(bookmarkService.selectBookmark(bookmarkVO) != null){
+            bookmarkService.deleteBookmark(bookmarkVO);
+        }
+        else{
+            bookmarkService.insertBookmark(bookmarkVO);
+        }
+
+        System.out.println("userID: " + bookmarkVO.getUserID() + "recipeID: " + bookmarkVO.getRecipeID());
+        
+        return bookmarkVO;
+    }
 }
