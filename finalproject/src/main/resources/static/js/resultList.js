6204729855d7c8c62ea27e2504185948be300eb3
JungHyukJin <hyukjin4976@naver.com>
2020-04-21 13:33:01 +0900

    중간 발표용

--- finalproject/src/main/resources/static/js/resultList.js
+++ finalproject/src/main/resources/static/js/resultList.js
@@ -3,8 +3,6 @@ const menuLists = document.getElementById("menu__lists");
 let i = "";
 let frag = document.createDocumentFragment();
 
-
-
 for (i in recipeList) {
   // console.log(i);
   let menuList = document.createElement("li");
@@ -15,43 +13,56 @@ for (i in recipeList) {
   let recipeTitle = document.createElement("h1");
   recipeTitle.innerHTML = recipeList[i].title;
   let recipeMain = document.createElement("span");
-  recipeMain.id = "recipe-main-ingredients"
+  recipeMain.id = "recipe-main-ingredients";
   recipeMain.innerHTML = `<span style="font-weight:bold";>ㅇ주재료:</span> ${recipeList[i].main}`;
   let recipeMinor = document.createElement("span");
-  recipeMinor.id = "recipe-minor-ingredients"
+  recipeMinor.id = "recipe-minor-ingredients";
   recipeMinor.innerHTML = `<span style="font-weight:bold";>ㅇ부재료:</span> ${recipeList[i].minor}`;
-  let recipeImg = document.createElement("div");
-  recipeImg.style.backgroundImage = `url('${recipeList[i].img_complete}')`
-  recipeImg.className = "menu__list-image";
-  recipeImg.href = "/recipe" + "/" + recipeList[i].id.toString()
-  recipeImg.addEventListener('click', function event() {
-    location.href = this.href
-  });
 
+
+  let recipeImgContainer = document.createElement("div")
+  recipeImgContainer.className = "recipe-img-container"
+  let recipeImgCover = document.createElement("div")
+  recipeImgCover.className = "recipe-img-cover"
+
+  let recipeImg = document.createElement("img");
+  recipeImgContainer.appendChild(recipeImgCover);
+  recipeImgCover.appendChild(recipeImg)
+
+  recipeImg.src = recipeList[i].img_complete;
+  recipeImg.className = "menu__list-image";
+
+  let bmImgCover = document.createElement("div");
+  bmImgCover.className = "bm-img-cover"
   let bmImg = document.createElement("img");
+  bmImgCover.appendChild(bmImg);
   bmImg.className = "bookmark-image"
   bmImg.src = "images/bm-logo.png";
   bmImg.addEventListener("click", addBookmark);
-  recipeImg.appendChild(bmImg);
+  recipeImgContainer.appendChild(bmImgCover);
 
-
+  menuList.appendChild(recipeID);
   menuList.appendChild(aboutMenu);
   aboutMenu.appendChild(recipeTitle);
   aboutMenu.appendChild(recipeMain);
   aboutMenu.appendChild(recipeMinor);
-  menuList.appendChild(recipeImg);
-
+  menuList.appendChild(recipeImgContainer);
 
   frag.appendChild(menuList);
   menuLists.appendChild(frag);
+
+  menuList.href = "/recipe" + "/" + recipeList[i].id.toString();
+  menuList.addEventListener("click", function event() {
+    location.href = this.href;
+  });
 }
 
 function addBookmark(event) {
   event.stopPropagation();
   const addBm = event.target;
-  if (addBm.getAttribute('src') == "images/bm-logo.png") {
-    addBm.setAttribute('src', 'images/bm-logo-checked.png');
-  } else if (addBm.getAttribute('src') == "images/bm-logo-checked.png") {
-    addBm.setAttribute('src', 'images/bm-logo.png');
+  if (addBm.getAttribute("src") == "images/bm-logo.png") {
+    addBm.setAttribute("src", "images/bm-logo-checked.png");
+  } else if (addBm.getAttribute("src") == "images/bm-logo-checked.png") {
+    addBm.setAttribute("src", "images/bm-logo.png");
   }
-};
+}
