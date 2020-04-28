570e6f3d0f1264019e157b50bffe2872068f1fc7
Yunok <ahyz0569@gmail.com>
2020-04-28 19:55:43 +0900

    detectron2 모델 사용을 위한 함수 적용

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -1,31 +1,36 @@
+## Library import
+# Flask
 from flask import Flask, jsonify, escape, request, render_template
 
+# Vectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+
+# For using detectron2 model
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.visualizer import Visualizer, ColorMode
+import cv2
+import requests
+
 from PIL import Image
 from io import BytesIO
-from torchvision import transforms
-from detecto import core, utils, visualize
+
+# SQL
 from sqlalchemy import create_engine
+# import mysql.connector
+
+# common library
+import xml.etree.ElementTree as elemTree
 import pandas as pd
-import xml.etree.ElementTree as elemTree
-import mysql.connector
 import numpy as np
-import xml.etree.ElementTree as elemTree
 import csv
 import pickle
-from detecto import core, utils, visualize
 import base64
-from PIL import Image
-from io import BytesIO
-import torch
-import torchvision
-from torchvision import transforms
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
-from sqlalchemy import create_engine
-import mysql.connector
 
 app = Flask(__name__)
 
@@ -80,35 +85,36 @@ def recommend(ingre_input, main):
     return df
 
 
-def show_labeled_image(image, boxes, labels=None, scores=None):
-    plt.rcParams.update({'font.size': 14})
-    fig, ax = plt.subplots(figsize=(20, 10))
-    fig.patch.set_visible(False)
-    ax.axis('off')
+# Detectron2 Model Predictor 생성 함수
+def prepare_predictor():
+    # create config
+    cfg = get_cfg()
+    
+    # cfgFile 경로 설정
+    cfgFile = "static/model/custom_config.yaml"
+    cfg.merge_from_file(cfgFile)
+    
+    ## customize config options
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
+    
+    # 이것도 학습시킨 모델 경로로 바꾸어 주어야함
+    cfg.MODEL.WEIGHTS = "static/model/model_final.pth"
+    cfg.MODEL.DEVICE = "cpu" # we use a CPU Detectron copy
+    
+    ingre_val_metadata = MetadataCatalog.get("my_dataset_val2", )
+    
+    # Metadata 생성을 위한 Data instance 생성
+    register_coco_instances("my_dataset_val_test", {},
+                        "static/model/output_val.json",
+                        '')
+    ingre_val_metadata = MetadataCatalog.get("my_dataset_val_test")
 
-    # If the image is already a tensor, convert it back to a PILImage and reverse normalize it
-    if isinstance(image, torch.Tensor):
-        image = reverse_normalize(image)
-        image = transforms.ToPILImage()(image)
-    ax.imshow(image)
+    # classes 형태: ['chilli', 'egg', 'porkmeat', 'potato', 'pa', 'onion', 'carrot', 'cucumber']
+    classes = MetadataCatalog.get(cfg.DATASETS.TEST[0]).thing_classes    
 
-    # Show a single box or multiple if provided
-    if boxes.ndim == 1:
-        boxes = boxes.view(1, 4)
-
-    if labels is not None and not utils._is_iterable(labels):
-        labels = [labels]
-
-    # Plot each box
-    for i in range(boxes.shape[0]):
-        box = boxes[i]
-        width, height = (box[2] - box[0]).item(), (box[3] - box[1]).item()
-        initial_pos = (box[0].item(), box[1].item())
-        rect = patches.Rectangle(initial_pos, width, height, linewidth=2.5, edgecolor='r', facecolor='none')
-        if labels:
-            ax.text(box[0] + 20, box[1] + 50, '{}'.format(labels[i]), color='r')
-        ax.add_patch(rect)
-    fig.savefig('static/images/detection_result.jpg', dpi=100)
+    predictor = DefaultPredictor(cfg)
+    print("Predictor has been initialized.")
+    return (predictor, classes, ingre_val_metadata)
 
 
 @app.route('/testapi', methods=["POST", "GET"])
@@ -119,19 +125,52 @@ def test():
     im = base64.b64decode(request.get_data())
     image = Image.open(BytesIO(im))
 
-    # 모든 객체를 찍음
-    ##predictions = model.predict(image)
+    # predictor 실행
+    outputs = predictor(image)
 
-    # 모든 객체중 정확도가 가장 높은 객체 감지
-    predictions = model.predict_top(image)
-    labels, boxes, scores = predictions
-    show_labeled_image(image, boxes, labels, scores)
+    # predictor 실행 확인
+    print(outputs)
+    
+    instances = outputs["instances"]    
+    pred_classes_idx = instances.get_fields()["pred_classes"].tolist()
+    # pred_boxes = instances.get_fields()["pred_boxes"].tensor.tolist()
+    # scores = instances.get_fields()["scores"].tolist()
 
-    with open('static/images/detection_result.jpg', 'rb') as img:
-        response_img = base64.b64encode(img.read())
-    print(type(response_img.decode('utf-8')))
+    v = Visualizer(image[:, :, ::-1],
+                   metadata=ingre_val_metadata, 
+                   scale=0.8
+    )
+    
+    # 재료 리스트를 한글로 변환하기 위한 dictionary 생성
+    # classes = 'chilli', 'egg', 'porkmeat', 'potato', 'pa', 'onion', 'carrot', 'cucumber'
+    ingre_dict ={0: '고추',
+                 1: '계란',
+                 2: '돼지고기',
+                 3: '감자',
+                 4: '파',
+                 5: '양파',
+                 6: '당근',
+                 7: '오이'
+                }
+    
+    # detect된 재료 class의 index 저장
+    pred_classes_idx = outputs["instances"].get_fields()["pred_classes"].tolist()
 
-    return jsonify(response_img=response_img.decode('utf-8'), labels=labels)
+    # 재료 리스트(한글)을 저장하기 위한 리스트 생성
+    pred_classes = []
+    for idx in pred_classes_idx:
+        pred_classes.append(ingre_dict[idx])
+
+    # 감지된 재료 리스트 한글 변환 확인용
+    print(pred_classes)
+
+    # 결과 이미지 생성
+    vis_output = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    print(type(vis_output))
+    # cv2_imshow(v.get_image()[:, :, ::-1])
+
+    return jsonify(response_img=vis_output.decode('utf-8'), labels=pred_classes)
+
 
 
 @app.route('/recomandApi', methods=["POST", "GET"])
@@ -146,23 +185,23 @@ def recomand():
         items.append(item.text)
         print('*' + item.text + '*')
 
-    for i in range(len(items)):
-        if (items[i] == "chilli"):
-            items[i] = "고추"
-        elif (items[i] == "egg"):
-            items[i] = "계란"
-        elif (items[i] == "pork meat"):
-            items[i] = "돼지고기"
-        elif (items[i] == "potato"):
-            items[i] = "감자"
-        elif (items[i] == "pa"):
-            items[i] = "파"
-        elif (items[i] == "onion"):
-            items[i] = "양파"
-        elif (items[i] == "carrot"):
-            items[i] = "당근"
-        elif (items[i] == "cucumber"):
-            items[i] = "오이"
+    # for i in range(len(items)):
+    #     if (items[i] == "chilli"):
+    #         items[i] = "고추"
+    #     elif (items[i] == "egg"):
+    #         items[i] = "계란"
+    #     elif (items[i] == "pork meat"):
+    #         items[i] = "돼지고기"
+    #     elif (items[i] == "potato"):
+    #         items[i] = "감자"
+    #     elif (items[i] == "pa"):
+    #         items[i] = "파"
+    #     elif (items[i] == "onion"):
+    #         items[i] = "양파"
+    #     elif (items[i] == "carrot"):
+    #         items[i] = "당근"
+    #     elif (items[i] == "cucumber"):
+    #         items[i] = "오이"
 
     items_KOR = " ".join(items)
     # print("하하하하: " + items_KOR)
@@ -226,6 +265,9 @@ if __name__ == '__main__':
     labels = ['chilli', 'egg', 'pork meat', 'potato', 'pa', 'onion', 'carrot', 'cucumber']
     vectorize = HashingVectorizer()
 
+    # detectron2 predictor 생성
+    predictor, classes, ingre_val_metadata = prepare_predictor()
+
     engine = create_engine('mysql://root:1234@localhost:3307/testDB?charset=utf8', convert_unicode=True,encoding='UTF-8')
     # engine = create_engine('mysql://JKS:12345678@sts.c2yt44rkrmcp.us-east-2.rds.amazonaws.com:3306/finalproject?charset=utf8', convert_unicode=True,encoding='UTF-8')
     conn = engine.connect()
@@ -235,9 +277,11 @@ if __name__ == '__main__':
     data["size"] = data['size'].astype("float")
     data["time"] = data['time'].astype("float")
     print(data.dtypes)
+
     ingre = data['ingre_main_oneline']
     ingre = np.array(ingre.tolist())
-    model = core.Model.load('static/model/detection_weights_v3.pth', labels)
+    
+    # model = core.Model.load('static/model/detection_weights_v3.pth', labels)
     # load model
     with open('static/model/hv.pkl', 'rb') as f:
         X = pickle.load(f)
